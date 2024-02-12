93a9b3eb4b979bf3b24e529817444e9accd85e19
dperaltaolivares502 <143421071+dperaltaolivares502@users.noreply.github.com>
2024-02-12 15:35:31 +0000

    hi

--- app.rb
+++ app.rb
@@ -2,36 +2,61 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1> Welcome to Rock-Paper-Scissors!</h1>
+  erb(:rules)
+end
+
+get("/rock") do
   
-  "
+moves = ["rock", "paper", "scissors"]
+
+@comp_move = moves.sample
+
+if @comp_move == "rock"
+  @outcome = "tied"
+elsif @comp_move == "paper"
+  @outcome = "lost"
+else
+  @outcome = "won"
 end
 
-# create a route for the homepage
-get '/' do
-  # render the homepage template
-  erb :index
+erb(:rock)
+
 end
 
-# create a post route to handle the form submission
-post '/play' do
-  # get the player's choice from the params
-  player_choice = params[:choice]
+get("/paper") do
+  
+  moves = ["rock", "paper", "scissors"]
+  
+  @comp_move = moves.sample
+  
+  if @comp_move == "paper"
+    @outcome = "tied"
+  elsif @comp_move == "scissors"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  
+  erb(:paper)
+  
+  end
 
-  # generate a random choice for the computer
-  computer_choice = ['rock', 'paper', 'scissors'].sample
 
-  # set the result variable to store the outcome
-  result = ""
-
-  # check for a tie
-  if player_choice == computer_choice
-    result = "It's a tie! You both chose #{player_choice}."
-  # check for player wins
-  elsif (player_choice == "rock" && computer_choice == "scissors") || (player_choice == "paper" && computer_choice == "rock") || (player_choice == "scissors" && computer_choice == "paper")
-    result = "Congratulations, you win! #{player_choice} beats #{computer_choice}."
-  # if the above conditions are not met, the computer wins
-  else 
-    result = "Sorry, you lost. #{computer_choice} beats #{player_choice}."
- 
+get("/scissors") do
+  
+  moves = ["rock", "paper", "scissors"]
+  
+  @comp_move = moves.sample
+  
+  if @comp_move == "scissors"
+    @outcome = "tied"
+  elsif @comp_move == "rock"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  
+  erb(:scissors)
+  
+  end
+  
